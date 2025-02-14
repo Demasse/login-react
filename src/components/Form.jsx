dc3f1fc171664400fed09600af8dbccaeeb8330e
Demasse <demassedimas2006@gmail.com>
2025-02-14 16:26:22 +0100

    form it self

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,40 @@ import * as React from 'react';
 export default function Form(){
     return (
         <div>
-            hello  forms
+          <h1 className=' text-5xl font-semibold'>Welcome Back</h1>
+          <p className=' font-medium text-lg text-gray-500 mt-4'>Welcome back § Please enter your details.</p>
+        <div className="">
+
+            <div className=""> 
+                <label >Email</label>
+                <input  
+                
+                className=''
+                placeholder='entre your email'
+                
+                />
+            </div>
+
+            <div className=""> 
+                <label >Password</label>
+                <input  
+                
+                className=''
+                placeholder='entre your password'
+                type='password'
+                
+                />
+            </div>
+            <div>
+                <div>
+                    <input type="checkbox" />
+                </div>
+                <label for="remember"> Rememer for 30 days </label>
+            </div>
+
+            <button> Forget password </button>
+
+        </div>
         </div>
     )
 }
