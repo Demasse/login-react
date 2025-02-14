fb3d3af9749deb8e2aa47954978677c408d22fae
Demasse <demassedimas2006@gmail.com>
2025-02-14 11:19:27 +0100

    begining of project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Forms from './components/form'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-red-500 text-3xl'> hello</h1>
+    
+    <Forms  />
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Forms(){
+    return (
+
+        
+        <div>
+        hello form </div>
+    
+    )
+
+}
